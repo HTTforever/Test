ef60464e1714198eea531dac6fde4e314522a085
saber <saber@qq.com>
2022-11-15 19:46:11 +0800

    first commit

---
+++ src/main/java/com/test/WebTest.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class WebTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git");
+        System.out.println("hello");
+        System.out.println("哈哈哈");
+    }
+}
