18b25adf7ac5f8d723a1ca4cd28e0dcc50225aa5
saber <saber@qq.com>
2022-11-15 20:01:14 +0800

    hot-fix first commit

--- src/main/java/com/test/WebTest.java
+++ src/main/java/com/test/WebTest.java
@@ -5,7 +5,6 @@ public class WebTest {
         System.out.println("Hello,Git");
         System.out.println("hello");
         System.out.println("哈哈哈");
-        System.out.println("试试看");
         System.out.println("我也不知道");
     }
 }
